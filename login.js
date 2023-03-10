be1ccd76b1430ba49adae72ee62cee2504e17a73
ksalomonson <kennysalomonson@msn.com>
2023-03-10 14:15:55 -0700

    Add javascript to play

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
+    localStorage.setItem("expierencePoints", 0);
+    localStorage.setItem("userLevel", 1);
     window.location.href = "play.html";
   }
   
